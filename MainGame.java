9541a00b8994f26f712fcd6ffc7822ba862916b5
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-03-19 19:59:33 -0400

    Create MainGame.java

---
+++ MainGame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainGame {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setTitle("Brick Breaker");
+        jFrame.setBounds(10, 10, 1000, 630);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
