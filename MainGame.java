2160f3403d1a2e2abfbadfe14481bdd16cb2c50a
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-03-26 15:03:37 -0400

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -7,8 +7,9 @@ public class MainGame {
         jFrame.setResizable(false);
         jFrame.setVisible(true);
         jFrame.setTitle("Brick Breaker");
-        jFrame.setBounds(10, 10, 1000, 630);
+        jFrame.setBounds(10, 10, 700, 600);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        Gameplay gameplay = new Gameplay();
+        jFrame.add(gameplay);
     }
 }
