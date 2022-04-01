751ae14e523773164d6e4c0d8b5a585a4cef51e0
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-04-01 18:50:45 -0400

    Update Gameplay.java

--- Gameplay.java
+++ Gameplay.java
@@ -2,9 +2,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class Gameplay extends JPanel implements ActionListener, KeyListener {
 
@@ -13,9 +12,9 @@ public class Gameplay extends JPanel implements ActionListener, KeyListener {
     int delay = 6;
     int score = 0;
 
-    int playerStartPosition = 310;
+    int playerXPosition = 310;
     int ballStartXPosition = 120;
-    int getBallStartYPosition = 350;
+    int ballStartYPosition = 350;
     int ballXDirection = -1;
     int ballYDirection = -2;
 
@@ -37,15 +36,35 @@ public class Gameplay extends JPanel implements ActionListener, KeyListener {
         g.fillRect(691, 0, 3  , 592);
 
         g.setColor(Color.green);
-        g.fillRect(playerStartPosition, 550, 100, 8);
+        g.fillRect(playerXPosition, 550, 100, 8);
 
         g.setColor(Color.yellow);
-        g.fillRect(ballStartXPosition, ballYDirection, 20, 20);
+        g.fillOval(ballStartXPosition, ballYDirection, 20, 20);
+
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.start();
+        if (gameStart) {
+            if (new Rectangle(ballStartXPosition, ballStartYPosition, 20, 20).intersects(new Rectangle(playerXPosition, 550, 100, 8))) {
+                ballYDirection = -ballYDirection;
+            }
+            ballStartXPosition += ballXDirection;
+            ballStartYPosition += ballStartYPosition;
+            if (ballStartXPosition < 0) {
+                ballXDirection = -ballXDirection;
+            }
+            if (ballStartYPosition < 0) {
+                ballYDirection = -ballYDirection;
+            }
+            if (ballStartXPosition > 670) {
+                ballXDirection = -ballXDirection;
+            }
+        }
 
+        repaint();
     }
 
     @Override
@@ -55,7 +74,30 @@ public class Gameplay extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerXPosition >= 600) {
+                playerXPosition = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerXPosition < 10) {
+                playerXPosition = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
 
+    public void moveRight() {
+        gameStart = true;
+        playerXPosition += 20;
+    }
+
+    public void moveLeft() {
+        gameStart = true;
+        playerXPosition -= 20;
     }
 
     @Override
